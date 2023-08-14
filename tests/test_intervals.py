45aa481ebbdf5eae109a1998329059d6e5b3aab1
DancingGrumpyCat <echo.zimet@gmail.com>
2023-08-14 10:18:51 -0400

    update test_truncate

--- tests/test_intervals.py
+++ tests/test_intervals.py
@@ -93,14 +93,16 @@ def test_contains() -> None:
 
 def test_truncate() -> None:
     x = Interval(0.21405899944813878, 8.463497115948577)
-    assert str(x.truncate(+100)) == "(0.21405899944813878, 8.463497115948577]"
-    assert str(x.truncate(+10)) == "(0.2140589994, 8.463497116]"
-    assert str(x.truncate(+5)) == "(0.21405, 8.4635]"
-    assert str(x.truncate(+1)) == "(0.2, 8.5]"
-    assert str(x.truncate()) == "(0, 9]"
-    assert str(x.truncate(0)) == "(0, 9]"
-    assert str(x.truncate(-1)) == "(0, 10]"
-    assert str(x.truncate(-2)) == "(0, 100]"
+    assert x.truncate(+100) == Interval.from_string(
+        "(0.21405899944813878, 8.463497115948577]"
+    )
+    assert x.truncate(+10) == Interval.from_string("(0.2140589994, 8.463497116]")
+    assert x.truncate(+5) == Interval.from_string("(0.21405, 8.4635]")
+    assert x.truncate(+1) == Interval.from_string("(0.2, 8.5]")
+    assert x.truncate() == Interval.from_string("(0, 9]")
+    assert x.truncate(0) == Interval.from_string("(0, 9]")
+    assert x.truncate(-1) == Interval.from_string("(0, 10]")
+    assert x.truncate(-2) == Interval.from_string("(0, 100]")
 
 
 def test_step() -> None:
