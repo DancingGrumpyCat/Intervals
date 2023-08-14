e0104e1989d9a38e1640420cb51621866f49a79a
DancingGrumpyCat <echo.zimet@gmail.com>
2023-08-14 18:10:17 -0400

    fix spacing (presentation)

--- src/intervals/constants.py
+++ src/intervals/constants.py
@@ -10,24 +10,28 @@ UNIT: Interval = Interval(
     lower_closure="open",
     upper_closure="closed",
 )
+
 NEGATIVE_UNIT: Interval = Interval(
     -1,
     +0,
     lower_closure="closed",
     upper_closure="open",
 )
+
 UNIT_DISK: Interval = Interval(
     -1,
     +1,
     lower_closure="closed",
     upper_closure="closed",
 )
+
 POSITIVE_REALS: Interval = Interval(
     +0,
     +_INF,
     lower_closure="open",
     upper_closure="closed",
 )
+
 NATURALS: Iterator[int] = map(int, POSITIVE_REALS.step(1))
 WHOLE_NUMBERS: Iterator[int] = map(int, (POSITIVE_REALS + 1).step(1))
 PI = Interval(223 / 71, 22 / 7, lower_closure="open", upper_closure="open")
